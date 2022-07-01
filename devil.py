bbfd135228e0128be64b74f5009438eee838be78
wilhelm <willo509@student.liu.se>
2022-07-01 17:41:40 +0200

    Fun object oriented prog

---
+++ devil.py
@@ -0,0 +1,9 @@
+class devil:
+
+    def __init__(self, name, fav_hobby, fav_color, age, is_busy_working) -> None:
+        self.name = name
+        self.fav_hobby = fav_hobby
+        self.fav_color = fav_color
+        self.age = age
+        self.is_busy_working = is_busy_working
+        
--- testMain.py
+++ testMain.py
@@ -1,7 +1,11 @@
-print("hello world!")
-print("hello world!")
-print("hello world!")
-print("hello world!")
-print("hello world!")
-print("hello world!")
-print("hello world!")
+import string
+from devil import devil
+
+devil1 = devil("Kurt", "Hiking", "Red", 32, False)
+devil2 = devil("Dennis", "Tennis", "Pink", 22, False)
+
+devil3 = devil("Bob", "Hiking", "Blue", 12, True)
+
+
+print(devil1.age)
+
