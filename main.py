726a8e7a8ae378e3ea4e0ef8ec0c8d689dc71e67
wilhelm <willo509@student.liu.se>
2022-07-02 19:09:46 +0200

    want to create a yt database, added abort function

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
+from email import message
 from flask import Flask
-from flask_restful import Api, Resource
-
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
-names = {"Wilhelm": {"age": 21, "gender": "male"},
-         "Kenta": {"age": 42, "gender": "male"}}
-class HelloWorld(Resource):
-    def get(self,name):
-        return names[name]
+videos = {}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+def abort_if_video_id_doesnt_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Could not find video..")
+
+
+class Video(Resource):
+    def get(self, video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        return videos[video_id]
+    
+    def put(self,video_id):
+        args = video_put_args.parse_args()
+        videos[video_id] = args
+        return videos[video_id], 201
+
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/Kenta")
+#response = requests.put(BASE + "video/1", {"likes": 21, "name": "crazzyKoding", "views": 4200})
+#print(response.json())
+
+#input()
+response = requests.get(BASE + "video/6")
 print(response.json())
