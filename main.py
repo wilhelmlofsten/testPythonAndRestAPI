57e2a73ac9261f3a1cc13a68c27dc9f89e53b47c
wilhelm <willo509@student.liu.se>
2022-07-02 18:11:53 +0200

    stored memory of names

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 api = Api(app)
 
 
-
+names = {"Wilhelm": {"age": 21, "gender": "male"},
+         "Kenta": {"age": 42, "gender": "male"}}
 class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Waaazzzaaaaaaaaaaaaap"}
+    def get(self,name):
+        return names[name]
 
-api.add_resource(HelloWorld, "/helloworld")
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+response = requests.get(BASE + "helloworld/Kenta")
 print(response.json())
