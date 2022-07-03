3b2d7c4219eb087f2bce3495d1e7d9dfc172bdfb
wilhelm <willo509@student.liu.se>
2022-07-03 20:26:03 +0200

    created a API Consumer that filters stackOverflow questions

--- main.py
+++ main.py
@@ -1,59 +1,18 @@
-from email import message
-from unicodedata import name # onödig??
-from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
+import requests
+import json
 
-app = Flask(__name__)
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-
-class VideoModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    views = db.Column(db.Integer, nullable=False)
-    likes = db.Column(db.Integer, nullable=False)
-
-    def __repr__ (self):
-        return f"Video(name = {name}, views = {views}, likes = {likes})"
+from sqlalchemy import false
 
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
-video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
-video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
 
 
-resource_fields = {
-    'id': fields.Integer,
-    'name': fields.String,
-    'views': fields.Integer,
-    'likes': fields.Integer
-}
-
-class Video(Resource):
-    @marshal_with(resource_fields)
-    def get(self, video_id):
-        result = VideoModel.query.filter_by(id=video_id).first()
-        return result
-    
-    @marshal_with(resource_fields)
-    def put(self,video_id):
-        args = video_put_args.parse_args()
-        result = VideoModel.query.filter_by(id=video_id).first()
-        if result:
-            abort(409, message="Video id is take..")
-        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-        db.session.add(video)
-        db.session.commit()
-        return video, 201
-
-   # def delete(self, video_id):
-        #del videos[video_id]
-       # return '', 204
-
-api.add_resource(Video, "/video/<int:video_id>")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+for data in response.json()['items']:
+    if data['answer_count'] ==0  and data['is_answered'] == False:
+        print("Title: " + data['title'])
+        print("score: ", data['score'])
+        print("view_count: ", data['view_count'])
+        print("link: " + data['link'])
+    else:
+        print("not worthy of my time..")
+    print("")
