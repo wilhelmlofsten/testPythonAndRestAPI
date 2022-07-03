af942270c5a86ef2f1e4ed3a995d8c1af26f6d73
wilhelm <willo509@student.liu.se>
2022-07-03 19:31:35 +0200

    added sqlAlchemy db

--- test.py
+++ test.py
@@ -10,9 +10,6 @@ for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), data[i])
     print(response.json())
 
-input()
-response = requests.delete(BASE + "video/0")
-print(response)
 
 input()
 response = requests.get(BASE + "video/2")
